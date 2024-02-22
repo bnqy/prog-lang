7e63f4f6a8506b7a4a848555a92e5e9e4fc6a298
Beneqi <obbonobo@gmail.com>
2024-02-22 21:47:45 +0600

    scanner

---
+++ j0.java
@@ -0,0 +1,35 @@
+import java.io.FileReader;
+public class j0 {
+   static Yylex lex;
+   public static int yylineno, yycolno;
+   public static token yylval;
+   public static void main(String argv[]) throws Exception {
+      lex = new Yylex(new FileReader(argv[0]));
+      yylineno = yycolno = 1;
+      int i;
+      while ((i=lex.yylex()) != Yylex.YYEOF) {
+         System.out.println("token " + i + ":"+yylineno+" " + yytext());
+      }
+   }
+   public static String yytext() {
+      return lex.yytext();
+   }
+   public static void lexErr(String s) {
+      System.err.println(s + ": line " + yylineno +
+                             ": " + yytext());
+      System.exit(1);
+   }
+   public static int scan(int cat) {
+      yylval = new token(cat, yytext(), yylineno, yycolno);
+      return cat;
+   }
+   public static void newline() {
+      yylineno++; yycolno = 1;
+   }
+   public static void whitespace() {
+       yycolno += yytext().length();
+   }
+   public static void comment() {
+   }
+   public static short ord(String s) { return (short)(s.charAt(0)); }
+}
--- test.java
+++ test.java
@@ -1,3 +1,4 @@
+
 import java.io.FileReader;
 
 public class test
